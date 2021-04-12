bfc5c4aa7c2f61d6db73c240611b10039891bc53
Sota Nakai <soooota1201@gmail.com>
2021-04-12 13:41:31 +0900

    add denim edit n update method.

---
+++ app/Http/Requests/Denims/UpdateDenimRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Denims;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateDenimRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'bland_type' => 'required',
+            'waist' => 'integer|digits:2',
+            'wearing_count' => 'integer',
+        ];
+    }
+}
--- resources/views/denim/edit.blade.php
+++ resources/views/denim/edit.blade.php
@@ -1,20 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
+
+@include('layouts._flash_message')
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <h2>デニムの情報を入力してください（Create, Store）</h2>
-
-            @if ($errors->any())
-              <div class="alert alert-danger">
-                <ul>
-                  @foreach ($errors->all() as $error)
-                    <li>{{$error}}</li>
-                  @endforeach
-                </ul>
-              </div><!-- /.alert alert-danger -->
-            @endif
 
             <form 
             method="POST" 
@@ -30,15 +23,15 @@
 
               <div class="form-group">
                 <label for="">ブランド</label>
-                <input type="text" class="form-control" placeholder="Levi's 501" name="bland_type">
+                <input type="text" class="form-control" placeholder="Levi's 501" name="bland_type" value="{{$denim->bland_type}}">
               </div>
               <div class="form-group">
                 <label for="">ウエスト（任意）</label>
-                <input type="text" class="form-control" placeholder="30" name="waist" value="">
+                <input type="text" class="form-control" placeholder="30" name="waist" value="{{$denim->waist}}">
               </div>
               <div class="form-group">
                 <label for="">履き込み回数（任意、新品は空欄）</label>
-                <input type="text" class="form-control" placeholder="30" name="wearing_count" value="">
+                <input type="text" class="form-control" placeholder="30" name="wearing_count" value="{{$denim->wearing_count}}">
               </div>
               
               <button type="submit" class="btn btn-primary">登録する</button>
