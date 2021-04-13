62f91880e151b68ef582c646f1e8b309ec3f4d2d
Sota Nakai <soooota1201@gmail.com>
2021-04-13 13:03:14 +0900

    add denim delete method

--- resources/views/denim/show.blade.php
+++ resources/views/denim/show.blade.php
@@ -25,9 +25,43 @@
                 </tr>
               </tbody>
             </table>
+
+            <form method="POST" action="{{ route('denims.destroy', $denim->id)}}">
+              @csrf
+
+              @method('DELETE')
+              
+                <!-- Modal -->
+                <div class="modal fade" id="modal{{$denim->id}}" tabindex="-1" role="dialog" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title">本当に削除しますか？</h5>
+                                <button type="button" class="close" data-dismiss="modal">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                {{$denim->title}}
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
+                                <button type="submit" class="btn btn-danger">
+                                    削除
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+
             
             <a href="{{route('denims.edit', $denim->id)}}" class="btn btn-lg btn-block btn-outline-dark">編集する</a>
-            <a href="{{route('denims.destroy', $denim->id)}}" class="btn btn-lg btn-block  btn-outline-dark mt4">削除する</a>
+            <a type="button" data-toggle="modal" data-target="#modal{{$denim->id}}" class="btn btn-lg btn-block  btn-outline-dark mt4">削除する</a>
+
+            
+
+            
         </div>
     </div>
 </div>
