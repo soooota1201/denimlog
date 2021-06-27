862ab65aff77dd556de1f8551c00e2f224f53787
Sota Nakai <soooota1201@gmail.com>
2021-06-27 17:25:38 +0900

    modified displaying google map in denim record page

--- resources/views/denim_records/show.blade.php
+++ resources/views/denim_records/show.blade.php
@@ -179,12 +179,12 @@
   function initMap() {
     geocoder = new google.maps.Geocoder();
     geocoder.geocode({
-        'address': '福岡' // TAM 東京
+        'address': @json($record->wearing_place)
     }, function(results, status) { // 結果
           if (status === google.maps.GeocoderStatus.OK) { // ステータスがOKの場合
             map = new google.maps.Map(document.getElementById('map'), {
               center: results[0].geometry.location, // 地図の中心を指定
-                zoom: 19 // 地図のズームを指定
+                zoom: 16 // 地図のズームを指定
             });
           marker = new google.maps.Marker({
             position: results[0].geometry.location, // マーカーを立てる位置を指定
