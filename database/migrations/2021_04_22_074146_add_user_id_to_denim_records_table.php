950932502f23083e21138840399e2773de9bdf50
Sota Nakai <soooota1201@gmail.com>
2021-07-23 18:45:07 +0900

    add new LP

--- database/migrations/2021_04_22_074146_add_user_id_to_denim_records_table.php
+++ database/migrations/2021_04_22_074146_add_user_id_to_denim_records_table.php
@@ -15,7 +15,7 @@ class AddUserIdToDenimRecordsTable extends Migration
     {
         Schema::table('denim_records', function (Blueprint $table) {
           $table->unsignedBigInteger('user_id');
-          $table->foreign('user_id')->references('id')->on('users');
+          $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
