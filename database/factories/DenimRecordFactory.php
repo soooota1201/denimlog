ad7406bc25ab8fa942cdc2e29626751644bfb731
Sota Nakai <soooota1201@gmail.com>
2021-12-10 23:17:42 +0900

    set up docker

--- database/factories/DenimRecordFactory.php
+++ database/factories/DenimRecordFactory.php
@@ -6,7 +6,12 @@ use App\DenimRecord;
 use Faker\Generator as Faker;
 
 $factory->define(DenimRecord::class, function (Faker $faker) {
+
     return [
-        
+        'user_id' => factory(App\User::class),
+        'wearing_day' => $faker->date,
+        'wearing_place' => 'Osaka',
+        'body' => $faker->text,
+        'bland_type' => factory(App\Denim::class)
     ];
 });
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,11 +17,13 @@ class UsersTableSeeder extends Seeder
             'password' => Hash::make('12345678'),
         ]);
 
-
         factory(App\User::class, 10)->create()
         ->each(function ($user) {
             $user->denims()->createMany(factory(App\Denim::class, 5)->make()->toArray()
             );
         });
+
+        $user = App\User::find(2);
+        dd($user->denims);
     }
 }
